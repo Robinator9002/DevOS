5d0d79abce25aad536b2e37242b4a8df2cf6fcc0
Robinator9002 <r.schumi09@gmail.com>
2025-09-20 14:41:40 +0200

    refactor(dungeon-class): use dungeon class in main loop

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -49,45 +49,20 @@ public:
 
 class Dungeon {
 public:
-    Dungeon(int rows, int cols, vector<vector<int>> walls) : rows(rows), cols(cols), walls(walls) {
+    Dungeon(int rows, int cols, vector<int> startingPos, vector<vector<int>> walls) : rows(rows), cols(cols), startingPos(startingPos), walls(walls) {
         CreateDungeon();
     }
 
     int rows;
     int cols;
-    vector<vector<int>> walls;
     Tile **grid;
     Player *player;
+    vector<int> startingPos;
+    vector<vector<int>> walls;
 
-private:
-    void CreateDungeon() {
-        // Create empty Grid
-        grid = new Tile *[rows];
-
-        // Fill Grid with Cols
-        for (int i = 0; i < rows; ++i)
-            grid[i] = new Tile[cols];
-    }
-
-    void InitDungeon() {
-        // Initialize Empty
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; ++j) {
-                grid[i][j].type = ' ';
-            }
-        }
-
-        // Initialize Walls
-        for (vector<int> wall : walls)
-            grid[wall[0]][wall[1]].type = '#';
-    }
-
-    void InitPlayer() {
-        // Get Position:
-        vector<int> pos = player->getPosition();
-
-        // Intialize Player
-        grid[pos[0]][pos[1]].type = 'P';
+    void Initialize() {
+        InitDungeon();
+        InitPlayer();
     }
 
     bool MovePlayer(const vector<int> &movement) {
@@ -115,14 +90,7 @@ private:
         return true;
     }
 
-    void PrintBorder() {
-        // A line of cols number of # plus 2 for corners
-        for (int j = 0; j < cols + 2; ++j)
-            cout << "#";
-        cout << "\n";
-    }
-
-    void PrintDungeon() {
+    void Print() {
         // Print out every Row and Column in the Dungeon, and borders at each side
         PrintBorder(); // Top border
         for (int i = 0; i < rows; ++i) {
@@ -145,6 +113,47 @@ private:
 
         // Clear Player
         delete[] player;
+    }
+
+private:
+    void CreateDungeon() {
+        // Create empty Grid
+        grid = new Tile *[rows];
+
+        // Fill Grid with Cols
+        for (int i = 0; i < rows; ++i)
+            grid[i] = new Tile[cols];
+    }
+
+    void InitDungeon() {
+        // Initialize Empty
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; ++j) {
+                grid[i][j].type = ' ';
+            }
+        }
+
+        // Initialize Walls
+        for (vector<int> wall : walls)
+            grid[wall[0]][wall[1]].type = '#';
+    }
+
+    void InitPlayer() {
+        // Create Player Class
+        player = new Player(startingPos);
+
+        // Get Position
+        vector<int> pos = player->getPosition();
+
+        // Intialize Player
+        grid[pos[0]][pos[1]].type = 'P';
+    }
+
+    void PrintBorder() {
+        // A line of cols number of # plus 2 for corners
+        for (int j = 0; j < cols + 2; ++j)
+            cout << "#";
+        cout << "\n";
     }
 };
 
@@ -181,24 +190,24 @@ vector<int> convertInputToMovement(char input) {
 int main() {
     // Variables
     int rows = 10, cols = 50;
+    vector<int> startingPos = {0, 0};
+    vector<vector<int>> walls = {{2, 3}};
 
     // Create and Initialize Dungeon and Player
-    Tile **dungeon = CreateDungeon(rows, cols);
-    InitDungeon(dungeon, rows, cols);
-    Player *player = new Player({0, 0});
-    InitPlayer(dungeon, player);
+    Dungeon *dungeon = &Dungeon(rows, cols, startingPos, walls);
+    dungeon->Initialize();
 
     // Print out the dungeon once at the beginning
-    PrintDungeon(dungeon, rows, cols);
+    dungeon->Print();
 
     // The main loop
     char input;
     while ((input = getch()) != 'q') {
         vector<int> movement = convertInputToMovement(input);
-        MovePlayer(dungeon, player, rows, cols, movement);
-        PrintDungeon(dungeon, rows, cols);
+        dungeon->MovePlayer(movement);
+        dungeon->Print();
     }
 
     // Cleanup afterwards
-    Cleanup(dungeon, rows, player);
+    dungeon->Cleanup();
 }
