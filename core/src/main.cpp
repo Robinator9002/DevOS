381892e59fc168dd50a4ca124072a23b52634485
Robinator9002 <r.schumi09@gmail.com>
2025-09-20 14:43:43 +0200

    feat(getch-function): move the getch function out to a new file

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -1,30 +1,14 @@
 // core/src/main.cpp
 
 #include <iostream>
-#include <termios.h>
-#include <unistd.h>
 #include <vector>
+
+// Import custom getch tool
+#include <getch.h>
 
 using namespace std;
 
-char getch() {
-    // Custom getch() function, cause linux doesnt support it directly
-    char buf = 0;
-    struct termios old = {0};
-    if (tcgetattr(0, &old) < 0)
-        perror("tcsetattr()");
-    old.c_lflag &= ~ICANON; // Disable buffered I/O
-    old.c_lflag &= ~ECHO;   // Disable echo
-    if (tcsetattr(0, TCSANOW, &old) < 0)
-        perror("tcsetattr ICANON");
-    if (read(0, &buf, 1) < 0)
-        perror("read()");
-    old.c_lflag |= ICANON;
-    old.c_lflag |= ECHO;
-    if (tcsetattr(0, TCSADRAIN, &old) < 0)
-        perror("tcsetattr ~ICANON");
-    return buf;
-}
+
 
 class Player {
 public:
